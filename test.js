d3b22efb4facd52a271ccb75984f41e75cada19f
Chandra Shekhar <100680681+ChandraShekhar1729@users.noreply.github.com>
2026-05-30 07:16:20 +0000

    basics of variables

--- test.js
+++ test.js
@@ -1,1 +1,29 @@
-console.log("This is chandrashekhar")
+console.log("This is chandrashekhar");
+
+const accountId = 144553;
+let accountEmail  = "chandrashekhar@gmail.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";// this is considered as which variable 
+let accountState // this is undefined
+
+// these are the variables 
+// const is  just assigned in the variable crartion post that nothing can be done 
+
+
+//accountId = 12432352142;//not allowed in this scenario for const you cant do this 
+
+//console.log(accountId);
+
+// let you can change the value but cant declare a vraible liek that again 
+
+ accountEmail = "1232452342";
+
+ console.log(accountEmail);
+
+var accountPassword =10;
+//{}---> these two braces is called scope
+
+
+ console.table([accountId,accountEmail,accountPassword,accountState])
+
+
